e1db9e4cd2d09b5abd2b1808260cff0f1b99967e
liujz <liujz@rc.inesa.com>
2019-11-21 10:44:13 +0800

    add cv part

---
+++ Day09-Unet/unet/metrics.py
@@ -0,0 +1,248 @@
+import numpy as np
+
+
+STYLE = {
+        'fore':
+        {   # 前景色
+            'black'    : 30,   #  黑色
+            'red'      : 31,   #  红色
+            'green'    : 32,   #  绿色
+            'yellow'   : 33,   #  黄色    
+            'blue'     : 34,   #  蓝色
+            'purple'   : 35,   #  紫红色
+            'cyan'     : 36,   #  青蓝色
+            'white'    : 37,   #  白色
+        },
+
+        'back' :
+        {   # 背景
+            'black'     : 40,  #  黑色
+            'red'       : 41,  #  红色
+            'green'     : 42,  #  绿色
+            'yellow'    : 43,  #  黄色
+            'blue'      : 44,  #  蓝色
+            'purple'    : 45,  #  紫红色
+            'cyan'      : 46,  #  青蓝色
+            'white'     : 47,  #  白色
+        },
+
+        'mode' :
+        {   # 显示模式
+            'mormal'    : 0,   #  终端默认设置
+            'bold'      : 1,   #  高亮显示
+            'underline' : 4,   #  使用下划线
+            'blink'     : 5,   #  闪烁
+            'invert'    : 7,   #  反白显示
+            'hide'      : 8,   #  不可见
+        },
+
+        'default' :
+        {
+            'end' : 0,
+        },
+}
+
+
+def UseStyle(string, mode = '', fore = '', back = ''):
+
+    mode  = '%s' % STYLE['mode'][mode] if STYLE['mode'].has_key(mode) else ''
+
+    fore  = '%s' % STYLE['fore'][fore] if STYLE['fore'].has_key(fore) else ''
+
+    back  = '%s' % STYLE['back'][back] if STYLE['back'].has_key(back) else ''
+
+    style = ';'.join([s for s in [mode, fore, back] if s])
+
+    style = '\033[%sm' % style if style else ''
+
+    end   = '\033[%sm' % STYLE['default']['end'] if style else ''
+
+    return '%s%s%s' % (style, string, end)
+
+def pixel_accuracy(eval_segm, gt_segm):
+    '''
+    sum_i(n_ii) / sum_i(t_i)
+    '''
+
+    check_size(eval_segm, gt_segm)
+
+    cl, n_cl = extract_classes(gt_segm)
+    eval_mask, gt_mask = extract_both_masks(eval_segm, gt_segm, cl, n_cl)
+
+    sum_n_ii = 0
+    sum_t_i = 0
+
+    for i, c in enumerate(cl):
+        curr_eval_mask = eval_mask[i, :, :]
+        curr_gt_mask = gt_mask[i, :, :]
+
+        sum_n_ii += np.sum(np.logical_and(curr_eval_mask, curr_gt_mask))
+        sum_t_i += np.sum(curr_gt_mask)
+
+    if (sum_t_i == 0):
+        pixel_accuracy_ = 0
+    else:
+        pixel_accuracy_ = sum_n_ii / sum_t_i
+
+    return pixel_accuracy_
+
+
+def mean_accuracy(eval_segm, gt_segm):
+    '''
+    (1/n_cl) sum_i(n_ii/t_i)
+    '''
+
+    check_size(eval_segm, gt_segm)
+
+    cl, n_cl = extract_classes(gt_segm)
+    eval_mask, gt_mask = extract_both_masks(eval_segm, gt_segm, cl, n_cl)
+
+    accuracy = list([0]) * n_cl
+
+    for i, c in enumerate(cl):
+        curr_eval_mask = eval_mask[i, :, :]
+        curr_gt_mask = gt_mask[i, :, :]
+
+        n_ii = np.sum(np.logical_and(curr_eval_mask, curr_gt_mask))
+        t_i = np.sum(curr_gt_mask)
+
+        if (t_i != 0):
+            accuracy[i] = n_ii / t_i
+
+    mean_accuracy_ = np.mean(accuracy)
+    return mean_accuracy_
+
+
+def mean_IU(eval_segm, gt_segm):
+    '''
+    (1/n_cl) * sum_i(n_ii / (t_i + sum_j(n_ji) - n_ii))
+    '''
+
+    check_size(eval_segm, gt_segm)
+
+    cl, n_cl = union_classes(eval_segm, gt_segm)
+    _, n_cl_gt = extract_classes(gt_segm)
+    eval_mask, gt_mask = extract_both_masks(eval_segm, gt_segm, cl, n_cl)
+
+    IU = list([0]) * n_cl
+
+    for i, c in enumerate(cl):
+        curr_eval_mask = eval_mask[i, :, :]
+        curr_gt_mask = gt_mask[i, :, :]
+
+        if (np.sum(curr_eval_mask) == 0) or (np.sum(curr_gt_mask) == 0):
+            continue
+
+        n_ii = np.sum(np.logical_and(curr_eval_mask, curr_gt_mask))
+        t_i = np.sum(curr_gt_mask)
+        n_ij = np.sum(curr_eval_mask)
+
+        IU[i] = n_ii / (t_i + n_ij - n_ii)
+
+    mean_IU_ = np.sum(IU) / n_cl_gt
+    return mean_IU_
+
+
+def frequency_weighted_IU(eval_segm, gt_segm):
+    '''
+    sum_k(t_k)^(-1) * sum_i((t_i*n_ii)/(t_i + sum_j(n_ji) - n_ii))
+    '''
+
+    check_size(eval_segm, gt_segm)
+
+    cl, n_cl = union_classes(eval_segm, gt_segm)
+    eval_mask, gt_mask = extract_both_masks(eval_segm, gt_segm, cl, n_cl)
+
+    frequency_weighted_IU_ = list([0]) * n_cl
+
+    for i, c in enumerate(cl):
+        curr_eval_mask = eval_mask[i, :, :]
+        curr_gt_mask = gt_mask[i, :, :]
+
+        if (np.sum(curr_eval_mask) == 0) or (np.sum(curr_gt_mask) == 0):
+            continue
+
+        n_ii = np.sum(np.logical_and(curr_eval_mask, curr_gt_mask))
+        t_i = np.sum(curr_gt_mask)
+        n_ij = np.sum(curr_eval_mask)
+
+        frequency_weighted_IU_[i] = (t_i * n_ii) / (t_i + n_ij - n_ii)
+
+    sum_k_t_k = get_pixel_area(eval_segm)
+
+    frequency_weighted_IU_ = np.sum(frequency_weighted_IU_) / sum_k_t_k
+    return frequency_weighted_IU_
+
+
+'''
+Auxiliary functions used during evaluation.
+'''
+
+
+def get_pixel_area(segm):
+    return segm.shape[0] * segm.shape[1]
+
+
+def extract_both_masks(eval_segm, gt_segm, cl, n_cl):
+    eval_mask = extract_masks(eval_segm, cl, n_cl)
+    gt_mask = extract_masks(gt_segm, cl, n_cl)
+
+    return eval_mask, gt_mask
+
+
+def extract_classes(segm):
+    cl = np.unique(segm)
+    n_cl = len(cl)
+
+    return cl, n_cl
+
+
+def union_classes(eval_segm, gt_segm):
+    eval_cl, _ = extract_classes(eval_segm)
+    gt_cl, _ = extract_classes(gt_segm)
+
+    cl = np.union1d(eval_cl, gt_cl)
+    n_cl = len(cl)
+
+    return cl, n_cl
+
+
+def extract_masks(segm, cl, n_cl):
+    h, w = segm_size(segm)
+    masks = np.zeros((n_cl, h, w))
+
+    for i, c in enumerate(cl):
+        masks[i, :, :] = segm == c
+
+    return masks
+
+
+def segm_size(segm):
+    try:
+        height = segm.shape[0]
+        width = segm.shape[1]
+    except IndexError:
+        raise
+
+    return height, width
+
+
+def check_size(eval_segm, gt_segm):
+    h_e, w_e = segm_size(eval_segm)
+    h_g, w_g = segm_size(gt_segm)
+
+    if (h_e != h_g) or (w_e != w_g):
+        raise EvalSegErr("DiffDim: Different dimensions of matrices!")
+
+
+'''
+Exceptions
+'''
+
+
+class EvalSegErr(Exception):
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return repr(self.value)
